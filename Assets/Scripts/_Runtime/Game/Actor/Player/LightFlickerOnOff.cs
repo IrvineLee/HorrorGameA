75851127ba22ceeaa09d5583d73ac11a3d34754a
IrvineLee <diabolo_555@yahoo.com>
2023-06-23 18:42:22 +0900

    Flickering point light

---
+++ Assets/Scripts/_Runtime/Game/Actor/Player/LightFlickerOnOff.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine;
+
+using Helper;
+
+namespace Personal.Character.Player
+{
+	public class LightFlickerOnOff : MonoBehaviour
+	{
+		[Tooltip("Minimum duration uptime")]
+		[SerializeField] float onMinDuration = 0f;
+		[Tooltip("Maximum duration uptime")]
+		[SerializeField] float onMaxDuration = 1f;
+		[Tooltip("Minimum duration downtime")]
+		[SerializeField] float offMinDuration = 0f;
+		[Tooltip("Maximum duration downtime")]
+		[SerializeField] float offMaxDuration = 1f;
+
+		new Light light;
+		CoroutineRun flickeringCR = new CoroutineRun();
+
+		void Start()
+		{
+			light = GetComponentInChildren<Light>();
+			Flicker(true);
+		}
+
+		public void Flicker(bool isFlag)
+		{
+			if (!isFlag)
+			{
+				flickeringCR.StopCoroutine();
+				return;
+			}
+
+			CoroutineRun waitCR = new CoroutineRun();
+			Action action = () =>
+			{
+				if (!waitCR.IsDone) return;
+
+				float random = UnityEngine.Random.Range(onMinDuration, onMaxDuration);
+				light.enabled = true;
+
+				waitCR = CoroutineHelper.WaitFor(random, () =>
+				{
+					if (flickeringCR.IsDone) return;
+					light.enabled = false;
+
+					random = UnityEngine.Random.Range(offMinDuration, offMaxDuration);
+					waitCR = CoroutineHelper.WaitFor(random, default);
+				});
+			};
+
+			flickeringCR = CoroutineHelper.RunActionUntilBreak(0, action, default);
+		}
+	}
+}
