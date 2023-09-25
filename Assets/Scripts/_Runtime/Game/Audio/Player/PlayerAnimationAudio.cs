d747aa0c67a3c4043d351644ef8c9305dff81454
IrvineLee <diabolo_555@yahoo.com>
2023-09-26 00:04:12 +0900

    Strafe with footstep sound

--- Assets/Scripts/_Runtime/Game/Audio/Player/PlayerAnimationAudio.cs
+++ Assets/Scripts/_Runtime/Game/Audio/Player/PlayerAnimationAudio.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Personal.Manager;
 using Personal.Setting.Audio;
 using Personal.Character.Player;
+using Helper;
 
 namespace Personal.Character.Animation
 {
@@ -12,9 +13,11 @@ namespace Personal.Character.Animation
 		[SerializeField] List<AudioSFXType> footstepSFXTypes = new List<AudioSFXType>();
 		[SerializeField] AudioSFXType landSFXType = AudioSFXType.PlayerLand;
 
+		[SerializeField] float footstepWeight = 0.02f;
 		[SerializeField] float footstepVolume = 0.5f;
 
 		FPSController fpsController;
+		CoroutineRun SEDelayCR = new();
 
 		protected override void Awake()
 		{
@@ -27,11 +30,13 @@ namespace Personal.Character.Animation
 		/// <param name="animationEvent"></param>
 		void OnFootstep(AnimationEvent animationEvent)
 		{
-			if (animationEvent.animatorClipInfo.weight <= 0.5f) return;
+			if (animationEvent.animatorClipInfo.weight <= footstepWeight) return;
 			if (footstepSFXTypes.Count <= 0) return;
+			if (!SEDelayCR.IsDone) return;
 
 			var index = Random.Range(0, footstepSFXTypes.Count);
 			AudioManager.Instance.PlaySFXAt(footstepSFXTypes[index], transform.TransformPoint(fpsController.Controller.center), footstepVolume);
+			SEDelayCR = CoroutineHelper.WaitFor(0.15f);
 		}
 
 		/// <summary>
