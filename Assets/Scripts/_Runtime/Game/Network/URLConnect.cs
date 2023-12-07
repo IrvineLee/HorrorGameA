b9c06c41d7269dbd598ee6de9b17496d0d63e99a
IrvineLee <diabolo_555@yahoo.com>
2023-12-08 01:35:17 +0900

    discord + twitter url jump

---
+++ Assets/Scripts/_Runtime/Game/Network/URLConnect.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Personal.Network
+{
+	public class URLConnect : MonoBehaviour
+	{
+		[SerializeField] string url = "";
+		[SerializeField] bool autoButtonEvent = true;
+
+		Button button;
+
+		void Awake()
+		{
+			if (!autoButtonEvent) return;
+
+			button = GetComponentInChildren<Button>();
+			button?.onClick.AddListener(Connect);
+		}
+
+		/// <summary>
+		/// Inspector : Attach this on the button event.
+		/// </summary>
+		public void Connect()
+		{
+			Application.OpenURL(url);
+		}
+
+		void OnDestroy()
+		{
+			button?.onClick.RemoveAllListeners();
+		}
+	}
+}
